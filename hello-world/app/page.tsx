732c0190baf63447344158b23dd3fd8702f0043c
Diego Carapia <diego.carapia@ufba.br>
2025-10-08 20:30:17 -0300

    corrigido

--- hello-world/app/page.tsx
+++ hello-world/app/page.tsx
@@ -1,9 +1,17 @@
-import Image from "next/image";
+'use client';
+import { useEffect,useState } from "react";
 
 export default function Home() {
+  const [message,setMessage] = useState("")
+
+  useEffect(()=>{
+    fetch('/api/hello').then(res => res.json())
+    .then(data =>setMessage(data.message))
+  },[])
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <h1>Hello World</h1>
+      <h1>{message}</h1>
     </div>
   );
 }
